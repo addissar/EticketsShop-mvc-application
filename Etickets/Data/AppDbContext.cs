4ff36a5ba57eb3ffe8cc4308ba53a89136fc58cb
Ziubin <ivan.ziubin@cruxlab.com>
2023-02-12 19:58:30 +0200

    add DbContext

---
+++ Etickets/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Etickets.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
