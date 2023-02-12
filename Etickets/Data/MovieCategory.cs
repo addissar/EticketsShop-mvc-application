ab2d139a7cb8a1436ead8b56f0280ae7ed4ee1e6
Ziubin <ivan.ziubin@cruxlab.com>
2023-02-12 19:45:39 +0200

    add All models

---
+++ Etickets/Data/MovieCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace Etickets.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
--- Etickets/Models/Producer.cs
+++ Etickets/Models/Producer.cs
@@ -1,6 +1,13 @@
-﻿namespace Etickets.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Etickets.Models
 {
     public class Producer
     {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
     }
 }
