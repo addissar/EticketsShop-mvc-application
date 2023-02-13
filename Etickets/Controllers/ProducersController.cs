e96f2387cc634e0dc3a936522a2618ba33da4d60
Ziubin <ivan.ziubin@cruxlab.com>
2023-02-13 18:24:31 +0200

    add Producers Controller

---
+++ Etickets/Controllers/ProducersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Etickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
